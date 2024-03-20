ae5570c0b94fd0d1118f24d66941931a1b9ff6e9
akhilManipal <akhil.menon@onlinemanipal.com>
2024-03-20 16:06:49 +0530

    Updated ConverterUtil and gradle File

---
+++ app/src/main/java/com/example/temperatureconversion/ConverterUtil.java
@@ -0,0 +1,7 @@
+package com.example.temperatureconversion;
+
+public class ConverterUtil {
+    public static double convertCelsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5.0) + 32;
+    }
+}
